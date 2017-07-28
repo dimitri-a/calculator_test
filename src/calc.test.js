2a240b52a546cdd61f018ec2876703d1f86656c7
Dimitri Couwenberg <couwenberg@gmail.com>
2017-07-28 19:20:43 +1000

    solved test

--- src/calc.test.js
+++ src/calc.test.js
@@ -3,15 +3,29 @@ import renderer from 'react-test-renderer';
 import ReactTestUtils from 'react-dom/test-utils'
 import Calculator from "./calculator";
 
-test('test that calculator', () => {
+test('test that calculator snapshot', () => {
     const component = renderer.create(
         <Calculator></Calculator>
     );
     let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
-   // console.log('component=',component.refs);
 
-    component.getInstance().calcSum(1,3);
     expect(tree).toMatchSnapshot();
+
+    let res = component.getInstance().calcSum(122,2);
+    
+
+
+});test('test that calculator again sum', () => {
+    const component = renderer.create(
+        <Calculator></Calculator>
+    );
+    let tree = component.toJSON();
+
+    let res = component.getInstance().calcSum(122,2);
+
+    console.log('res=',res);
+
+    expect(res).toEqual(124);
+
 
 });
--- src/calculator.js
+++ src/calculator.js
@@ -4,21 +4,15 @@ export default class Calculator extends Component {
     constructor(props) {
         super(props);
         this.calcSum = this.calcSum.bind(this);
-        this.go = this.go.bind(this);
         this.state = {sum: 0};
     }
 
-    go()
-    {
-        console.log('this.refs.one=', this.refs.one);
-        this.calcSum(this.refs.one.value, this.refs.two.value)
-    }
 
     calcSum(s1, s2) {
-        console.log('this.refs.one=', this.refs.one);
+        //console.log('this.refs.one=', this.refs.one);
         let s = Number(s1) + Number(s2);
 
-        this.setState({sum: s});
+       return s;
     }
 
     render() {
@@ -26,7 +20,7 @@ export default class Calculator extends Component {
                 <input type="text" placeholder="number 1" ref="one"/>
                 <input type="text" placeholder="number 2" ref="two"/>
 
-                <button ref="button" onClick={this.go}>sum</button>
+                <button ref="button" onClick={this.calcSum(this.refs.one,this.refs.two)}>sum</button>
                 sum: {this.state.sum}
 
             </div>
